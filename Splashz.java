71c6fb2d5bbb9d3d5461c45de09a53d9d79e835d
mjpllano <72667996+MJesusGit@users.noreply.github.com>
2020-12-13 13:53:46 +0100

    Splash
    Ventana de carga

---
+++ Splashz.java
@@ -0,0 +1,86 @@
+package presentacion;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
+import java.awt.Color;
+import javax.swing.JProgressBar;
+import java.awt.Font;
+
+public class Splashz extends JFrame  {
+
+	private JPanel contentPane;
+	private JLabel lblCargando;
+	private JProgressBar progressBar;
+
+	/**
+	 * Launch the application.
+	 */
+	
+	/**
+	 * Create the frame.
+	 */
+	public Splashz() {
+		setUndecorated(true);
+		setBounds(100, 100, 894, 607);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JPanel pnlcontenido = new JPanel();
+		pnlcontenido.setBorder(null);
+		pnlcontenido.setBackground(new Color(255, 153, 102));
+		contentPane.add(pnlcontenido, BorderLayout.CENTER);
+		pnlcontenido.setLayout(null);
+		
+		lblCargando = new JLabel("");
+		lblCargando.setIcon(new ImageIcon(Splashz.class.getResource("/presentacion/Vanilla-1.7s-267px.gif")));
+		lblCargando.setHorizontalAlignment(SwingConstants.CENTER);
+		lblCargando.setBounds(183, 456, 520, 71);
+		pnlcontenido.add(lblCargando);
+		
+		progressBar = new JProgressBar();
+		progressBar.setStringPainted(true);
+		progressBar.setBackground(new Color(255, 228, 196));
+		progressBar.setForeground(new Color(255, 102, 0));
+		progressBar.setBounds(183, 527, 520, 45);
+		pnlcontenido.add(progressBar);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setForeground(new Color(255, 69, 0));
+		lblNewLabel.setFont(new Font("Verdana", Font.BOLD, 22));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setIcon(new ImageIcon(Splashz.class.getResource("/presentacion/SplashFoxycamp.png")));
+		lblNewLabel.setBounds(0, 0, 884, 597);
+		pnlcontenido.add(lblNewLabel);
+		
+	}
+	
+	public static void main(String[] args) {
+		Splashz splash = new Splashz ();
+		splash.setVisible(true);
+		try {
+			
+			for(int i=0;i<=100;i++) {
+				Thread.sleep(50);
+				splash.progressBar.setValue(i);
+				if(i==100) {
+					P_Login login = new P_Login();
+					splash.setVisible(false);
+					login.setVisible(true);
+					
+				}
+			}
+		}catch(Exception e){
+			
+		}
+	}
+	
+}
