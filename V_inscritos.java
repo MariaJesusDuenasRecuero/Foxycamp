14c1af42869a2cebaf4775b8410108aac5a2c4d3
mjpllano <72667996+MJesusGit@users.noreply.github.com>
2021-01-02 21:37:09 +0100

    Add files via upload

--- V_inscritos.java
+++ V_inscritos.java
@@ -12,6 +12,10 @@ import java.awt.Font;
 import javax.swing.border.TitledBorder;
 import javax.swing.border.EtchedBorder;
 import java.awt.Color;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.ScrollPaneConstants;
 
 public class V_inscritos extends JPanel {
 	private JTable TableInscritos;
@@ -29,10 +33,12 @@ public class V_inscritos extends JPanel {
 		panel.setLayout(null);
 		
 		scrollPane = new JScrollPane();
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollPane.setBounds(0, 0, 1147, 611);
 		scrollPane.setFont(new Font("Verdana", Font.PLAIN, 16));
 		scrollPane.setAutoscrolls(true);
 		scrollPane.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "Inscritos a las actividades", TitledBorder.LEADING, TitledBorder.TOP, null, Color.BLACK));
-		scrollPane.setBounds(0, 0, 1001, 530);
 		panel.add(scrollPane);
 		
 		TableInscritos = new JTable();
@@ -43,14 +49,11 @@ public class V_inscritos extends JPanel {
 		TableInscritos.getTableHeader().setFont(new Font("Verdana",Font.BOLD,16));
 		TableInscritos.getTableHeader().setBackground(new Color(255, 165, 0));
 		TableInscritos.getTableHeader().setForeground( Color.BLACK);
-		
-				
-		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-		//TableInscritos.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		TableInscritos.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 		TableInscritos.setAutoCreateRowSorter(true);
 		TableInscritos.setModel(new DefaultTableModel(
 			new Object[][] {
-				{null, null, null, null, null, null, null, null, null, null},
+				{"NATACI\u00D3N", "TARDE", "JUAN", "MARTINEZ RUIZ", "05964229Z", "juanm@gmail.com", new Integer(926568526), null, Boolean.TRUE, "Diab\u00E9tico"},
 				{null, null, null, null, null, null, null, null, null, null},
 				{null, null, null, null, null, null, null, null, null, null},
 				{null, null, null, null, null, null, null, null, null, null},
@@ -128,13 +131,19 @@ public class V_inscritos extends JPanel {
 			}
 		});
 		TableInscritos.getColumnModel().getColumn(0).setResizable(false);
-		TableInscritos.getColumnModel().getColumn(4).setPreferredWidth(69);
+		TableInscritos.getColumnModel().getColumn(0).setPreferredWidth(160);
+		TableInscritos.getColumnModel().getColumn(1).setPreferredWidth(166);
+		TableInscritos.getColumnModel().getColumn(2).setPreferredWidth(194);
+		TableInscritos.getColumnModel().getColumn(3).setResizable(false);
+		TableInscritos.getColumnModel().getColumn(3).setPreferredWidth(179);
+		TableInscritos.getColumnModel().getColumn(4).setPreferredWidth(194);
+		TableInscritos.getColumnModel().getColumn(4).setMinWidth(16);
 		TableInscritos.getColumnModel().getColumn(5).setResizable(false);
-		TableInscritos.getColumnModel().getColumn(5).setPreferredWidth(99);
-		TableInscritos.getColumnModel().getColumn(6).setPreferredWidth(63);
-		TableInscritos.getColumnModel().getColumn(7).setPreferredWidth(47);
+		TableInscritos.getColumnModel().getColumn(5).setPreferredWidth(215);
+		TableInscritos.getColumnModel().getColumn(6).setPreferredWidth(211);
+		TableInscritos.getColumnModel().getColumn(7).setPreferredWidth(152);
 		TableInscritos.getColumnModel().getColumn(8).setPreferredWidth(48);
-		TableInscritos.getColumnModel().getColumn(9).setPreferredWidth(114);
+		TableInscritos.getColumnModel().getColumn(9).setPreferredWidth(273);
 		scrollPane.setViewportView(TableInscritos);
 		
 		
