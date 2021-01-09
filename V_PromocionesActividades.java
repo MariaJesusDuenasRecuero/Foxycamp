584bd4325e233cdadefb98443fcd47bb7444169a
mjpllano <72667996+MJesusGit@users.noreply.github.com>
2021-01-09 15:09:08 +0100

    Add files via upload

--- V_PromocionesActividades.java
+++ V_PromocionesActividades.java
@@ -44,7 +44,7 @@ public class V_PromocionesActividades extends JPanel {
 		
 		JLabel lblIconOferta1 = new JLabel("");
 		lblIconOferta1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblIconOferta1.setIcon(new ImageIcon(V_PromocionesActividades.class.getResource("/presentacion/IconCalendario.png")));
+		lblIconOferta1.setIcon(new ImageIcon(V_PromocionesActividades.class.getResource("/presentacion/IconPromo2.png")));
 		lblIconOferta1.setBounds(32, 367, 102, 93);
 		pnlContenido.add(lblIconOferta1);
 		
@@ -92,7 +92,7 @@ public class V_PromocionesActividades extends JPanel {
 		
 		JButton btnInscribirse = new JButton("Inscribirse");
 		btnInscribirse.setBounds(new Rectangle(0, 0, 60, 50));
-		btnInscribirse.setFont(new Font("Verdana", Font.PLAIN, 16));
+		btnInscribirse.setFont(new Font("Verdana", Font.BOLD, 16));
 		btnInscribirse.setBackground(new Color(255, 165, 0));
 		btnInscribirse.setBounds(433, 469, 124, 23);
 		btnInscribirse.setBorder(new RoundedBorder(10));
